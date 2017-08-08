3b3e56c78887ec1f7af1744cf3540c9e56b7e363
Paul Klemm <paul.klemm@sf.mpg.de>
2017-08-08 17:36:35 +0200

    Add to_go function generalizing gene and transcript input
    Additionally add function to switch the ensembl id

--- R/go.R
+++ R/go.R
@@ -1,12 +1,40 @@
 #   Build and Reload Package:  'Cmd + Shift + B'
 #   Check Package:             'Cmd + Shift + E'
 #   Test Package:              'Cmd + Shift + T'
-pkg.env <- new.env()
-pkg.env$dataset <- "mmusculus_gene_ensembl"
-pkg.env$ensembl <- useMart("ensembl",dataset=pkg.env$dataset)
-pkg.env$gene_and_go <- getBM(attributes=c('ensembl_gene_id', 'go_id'), mart = pkg.env$ensembl)
-# pkg.env$transcript_to_go <- getBM(attributes=c('ensembl_transcript_id', 'go_id'), mart = pkg.env$ensembl)
 
+#' @title Set Ensembl dataset for the package
+#'
+#' @param ensembl_dataset Ensembl dataset name
+#' @export
+set_ensembl_dataset <- function(ensembl_dataset) {
+  pkg.env <<- new.env()
+  pkg.env$dataset <- ensembl_dataset
+  pkg.env$ensembl <- useMart("ensembl",dataset=pkg.env$dataset)
+  pkg.env$gene_and_go <- getBM(attributes=c('ensembl_gene_id', 'go_id'), mart = pkg.env$ensembl)
+  # pkg.env$transcript_to_go <- getBM(attributes=c('ensembl_transcript_id', 'go_id'), mart = pkg.env$ensembl)
+}
+
+#' @title Get the GO term association per gene or transcript as list
+#'
+#' @param input Vector of Ensembl gene or transcript ids
+#' @return List named by gene/transcript id containing a vector of associated GO terms by accession id
+#' @example to_go(c("ENSMUTG00000064370", "ENSMUST00000065947"))
+#' @export
+to_go <- function(input) {
+  # Convert input to uppercase
+  input <- toupper(input)
+  # Try to guess whether input is genes or transcripts
+  # Ensembl Genes end with `G` followed by the id number, transcripts with a `t`, e.g. 'ENSMUSG00000064370'
+  if (sum(grepl('(G[0-9]*$)', input)) == length(input)) {
+    # Process as Genes
+    return(gene_to_go(input))
+  } else if (sum(grepl('(T[0-9]*$)', input)) == length(input)) {
+    # TODO
+    # Process as Transcripts.
+  } else {
+    stop('Input must be Ensembl genes or transcripts')
+  }
+}
 
 #' @title Get the GO term association per gene as list
 #'
@@ -14,7 +42,6 @@ pkg.env$gene_and_go <- getBM(attributes=c('ensembl_gene_id', 'go_id'), mart = pk
 #' @import dplyr
 #' @param genes Vector of Ensembl gene names
 #' @return List named by gene id containing a vector of associated GO terms by accession id
-#' @export
 gene_to_go <- function(genes) {
   result <- list()
   for (gene in genes) {
@@ -22,3 +49,6 @@ gene_to_go <- function(genes) {
   }
   return(result)
 }
+
+# Set default environment to mouse genes
+set_ensembl_dataset('mmusculus_gene_ensembl')
