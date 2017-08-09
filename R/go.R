32b19db19109d928353c3a71ba2fd412b1615157
Paul Klemm <paul.klemm@sf.mpg.de>
2017-08-09 12:42:13 +0200

    Add new download function that is dynamic on species and ensembl version

--- R/go.R
+++ R/go.R
@@ -2,6 +2,9 @@
 #   Check Package:             'Cmd + Shift + E'
 #   Test Package:              'Cmd + Shift + T'
 
+debug <- FALSE
+debug_path <- '~/Downloads/ensembl'
+
 get_ensembl_dataset <- function(ensembl_dataset='mmusculus_gene_ensembl', version='current') {
   # Check if the ensembl folder exists
   if (system.file("ensembl", package="sonaRGO") == '')
@@ -13,7 +16,10 @@ get_ensembl_dataset <- function(ensembl_dataset='mmusculus_gene_ensembl', versio
   }
 
   # Construct file name
-  ensembl_path <- file.path(path.package('sonaRGO'), 'ensembl', paste0(ensembl_dataset, '_', version, '.rdmp'))
+  if (debug)
+    ensembl_path <- file.path(debug_path, paste0(ensembl_dataset, '_', version, '.rdmp'))
+  else
+    ensembl_path <- file.path(path.package('sonaRGO'), 'ensembl', paste0(ensembl_dataset, '_', version, '.rdmp'))
   # Check if the file exists
   if (!file.exists(ensembl_path)) {
     # Get the biomart
@@ -23,7 +29,7 @@ get_ensembl_dataset <- function(ensembl_dataset='mmusculus_gene_ensembl', versio
   } else {
     load(file = ensembl_path)
   }
-  # TODO return the stuff
+  return(list(gene_and_go = gene_and_go))
 }
 
 #' @title Set Ensembl dataset for the package
@@ -41,17 +47,22 @@ set_ensembl_dataset <- function(ensembl_dataset) {
 #' @title Get the GO term association per gene or transcript as list
 #'
 #' @param input Vector of Ensembl gene or transcript ids
+#' @param ensembl_dataset Ensembl dataset name specifying the species
+#' @param ensembl_version Ensembl version. Defaults to 'current'
 #' @return List named by gene/transcript id containing a vector of associated GO terms by accession id
-#' @example to_go(c("ENSMUTG00000064370", "ENSMUST00000065947"))
+#' @example to_go(c("ENSMUSG00000064370", "ENSMUSG00000065947"), 'mmusculus_gene_ensembl')
 #' @export
-to_go <- function(input) {
+to_go <- function(input, ensembl_dataset, ensembl_version = 'current') {
+  # Get the required data frames
+  ensembl_go_data <- get_ensembl_dataset(ensembl_dataset = ensembl_dataset, version = ensembl_version)
+  gene_and_go <- ensembl_go_data$gene_and_go
   # Convert input to uppercase
   input <- toupper(input)
   # Try to guess whether input is genes or transcripts
   # Ensembl Genes end with `G` followed by the id number, transcripts with a `t`, e.g. 'ENSMUSG00000064370'
   if (sum(grepl('(G[0-9]*$)', input)) == length(input)) {
     # Process as Genes
-    return(gene_to_go(input))
+    return(gene_to_go(input, gene_and_go))
   } else if (sum(grepl('(T[0-9]*$)', input)) == length(input)) {
     # TODO
     # Process as Transcripts.
@@ -66,13 +77,14 @@ to_go <- function(input) {
 #' @import dplyr
 #' @param genes Vector of Ensembl gene names
 #' @return List named by gene id containing a vector of associated GO terms by accession id
-gene_to_go <- function(genes) {
+gene_to_go <- function(genes, gene_and_go) {
+  # Populate result
   result <- list()
   for (gene in genes) {
-    result[gene] <- pkg.env$gene_and_go %>% filter(ensembl_gene_id == gene) %>% select(go_id) %>% as.vector()
+    result[gene] <- gene_and_go %>% filter(ensembl_gene_id == gene) %>% select(go_id) %>% as.vector()
   }
   return(result)
 }
 
 # Set default environment to mouse genes
-set_ensembl_dataset('mmusculus_gene_ensembl')
+# set_ensembl_dataset('mmusculus_gene_ensembl')
